5ed09edaf9cdae0a854f0c35388b05355a3930d6
YeahYo17 <starkgs@hotmail.com>
2016-03-15 01:50:00 +0100

    Inserción Archivos

---
+++ PaginaRegistro/paginaregistro/InsertarTarea.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+
+Public Class InsertarTarea
+
+    Inherits System.Web.UI.Page
+
+    Private dAdapter As SqlDataAdapter
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Session("dAdapter") = dAdapter
+    End Sub
+
+    Protected Sub btnAñadir_Click(sender As Object, e As EventArgs) Handles btnAñadir.Click
+
+        Dim dTable As DataTable
+        Dim dSet As DataSet = New DataSet()
+
+        Dim strSql As String
+
+        strSql = "SELECT * FROM TareasGenericas"
+        dAdapter = New SqlDataAdapter(strSql, Session("Conexion"))
+
+
+
+
+    End Sub
+End Class
---
+++ PaginaRegistro/paginaregistro/TareasProfesor.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+        Response.Redirect("InsertarTarea.aspx")
+    End Sub
+
+End Class
